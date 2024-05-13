e7ae8db2e8cc4109052ea25ebb80a29e55272e9b
Oskar Wang <80847085+owhygithub@users.noreply.github.com>
2024-05-13 23:55:56 +0200

    Code cleaned + reorganization
    Code cleaned + reorganization

---
+++ todo.py
@@ -0,0 +1,27 @@
+# PLAN MONDAY 
+# FIX Model input Data 
+# TODO Create equivalent variables but flip nodes with edges 
+
+# DONE Understand architecture 
+# DONE Understand model input data 
+# DONE There are missing connections in the adjacency matrix # IDEA scrap adjacency matrix entirely if necessary? 
+# DONE Create model without NeighborSampler 
+# DONE Clean code 
+# DONE Make model work ... 
+
+# PLAN TUESDAY 
+# TODO ADMIN - Send email with Update (ask about servers?) 
+# TODO CODE - Make model more extensive 
+# TODO CODE - Visualize prediction patterns and learning curve for existing model 
+# TODO CODE - Create testing stage and evaluation 
+# TODO THEORY - Watch GNN Theory 
+# TODO THEORY - Write up Introduction 
+
+# PLAN WEDNESDAY 
+# TODO THEORY - Understand Math Theory 
+# TODO THEORY - Write up methodology theory 
+# TODO THEORY - Write up Literature Review 
+
+# PLAN THURSDAY 
+# TODO CODE - add statistics to a new DataFrame 
+# TODO CODE - add average of transactions for accounts 
