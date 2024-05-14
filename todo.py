e5945b620cd247633e173aa22b77a45a0cc68df2
Oskar Wang <80847085+owhygithub@users.noreply.github.com>
2024-05-14 23:11:57 +0200

    updated todo list
    updated todo list

--- todo.py
+++ todo.py
@@ -1,27 +1,42 @@
 # PLAN MONDAY 
-# FIX Model input Data 
-# TODO Create equivalent variables but flip nodes with edges 
+# DONE Model input Data 
+# DONE Create equivalent variables but flip nodes with edges 
 
-# DONE Understand architecture 
-# DONE Understand model input data 
-# DONE There are missing connections in the adjacency matrix # IDEA scrap adjacency matrix entirely if necessary? 
-# DONE Create model without NeighborSampler 
-# DONE Clean code 
-# DONE Make model work ... 
+# DONE THEORY - Understand architecture 
+# DONE CODE - Understand model input data 
+# DONE CODE - There are missing connections in the adjacency matrix # IDEA scrap adjacency matrix entirely if necessary? 
+# DONE CODE - Create model without NeighborSampler 
+# DONE CODE - Clean code 
 
 # PLAN TUESDAY 
-# TODO ADMIN - Send email with Update (ask about servers?) 
-# TODO CODE - Make model more extensive 
-# TODO CODE - Visualize prediction patterns and learning curve for existing model 
-# TODO CODE - Create testing stage and evaluation 
+# DONE CODE - Make model work ... 
+# DONE CODE - Make model more extensive --> further cleaning of code 
 
 # PLAN WEDNESDAY 
-# TODO THEORY - Watch GNN Theory 
+# TODO CODE - finish classes for creating graphs 
+# TODO CODE - implement edge features to the GCN 
+# TODO CODE - run to create usable data for evaluations 
+
+# TODO Make some questions for Meeting 
+# TODO ADMIN - Send email with Update (ask about servers?) 
+
+# TODO CODE - Visualize Training (patterns and learning curve)
+# TODO CODE - Create testing stage and evaluation (using new py file) 
+
+# PLAN THURSDAY 
+# TODO THEORY - Watch GNN Theory --> fully understand it
+# TODO THEORY - Find spots for Servers
 # TODO THEORY - Write up Introduction 
+# FIX THESIS MEETING
+
+# TODO THEORY - Understand Code
 # TODO THEORY - Understand Math Theory 
 # TODO THEORY - Write up methodology theory 
 # TODO THEORY - Write up Literature Review 
 
-# PLAN THURSDAY 
+
+
+
+
 # TODO CODE - add statistics to a new DataFrame 
 # TODO CODE - add average of transactions for accounts 
