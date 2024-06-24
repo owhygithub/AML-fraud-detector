2e3c08775ec77e8d8f9646c8811a88b0ff8b11ae
Oskar Wang <80847085+owhygithub@users.noreply.github.com>
2024-06-24 21:47:54 +0200

    a

--- Py Scripts/1b_distmult_t.py
+++ Py Scripts/1b_distmult_t.py
@@ -219,7 +219,7 @@ class GNNModel(nn.Module):
 learning_rate = 0.01
 out_channels = 10
 weight_decay = 0.0005  # L2 regularization factor
-epochs = 50
+epochs = 200
 dropout = 0.1 # dropout probability
 
 # Annealing parameters
