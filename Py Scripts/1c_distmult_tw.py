2a046e707d322f0703255e4747461541c8982684
Oskar Wang <80847085+owhygithub@users.noreply.github.com>
2024-06-24 21:42:17 +0200

    Update 1c_distmult_tw.py

--- Py Scripts/1c_distmult_tw.py
+++ Py Scripts/1c_distmult_tw.py
@@ -64,7 +64,7 @@ def calculate_mrr(sorted_indices, true_values):
 
 print("Started the program...")
 # Specify the file path where the data is saved
-file_path = "/var/scratch/hwg580/graph_HI-Small_Trans_balanced.csv.pickle"
+file_path = "/var/scratch/hwg580/graph_HI-Small_Trans_balanced.pickle"
 
 # Load the data from the file
 with open(file_path, "rb") as f:
